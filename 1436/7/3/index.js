3445138f99719948cff8226e1aa846632d9aad76
Will Holcomb <will@dhappy.org>
2015-04-23 12:04:03 -0400

    adding progress bar

--- 1436/7/3/index.js
+++ 1436/7/3/index.js
@@ -1,7 +1,26 @@
 $(function() {
-    $('#video').on( 'timeupdate', function( event ) {
-	if( this.currentTime > 10 ) {
-	    Reveal.right()
+    function syncSlide( time ) {
+	if( time < 5 ) {
+	    Reveal.slide( 0 )
+	} else if( time < 10 ) {
+	    Reveal.slide( 1 )
+	} else if( time < 15 ) {
+	    Reveal.slide( 2 )
+	} else if( time < 20 ) {
+	    Reveal.slide( 3 )
+	} else if( time < 25 ) {
+	    Reveal.slide( 4 )
+	} else if( time < 30 ) {
+	    Reveal.slide( 5 )
 	}
+    }
+
+    $('#video').on( 'timeupdate', function( event ) {
+	var time = this.currentTime
+	syncSlide( time )
+	
     } )
+
+    var $progressBar = $('<div/>').attr( { id: 'progress' } )
+    $('body').prepend( $progressBar )
 } )
