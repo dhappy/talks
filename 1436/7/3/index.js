09bdd6cf7e5cd33fb56a8bbabc1852401a376174
Will Holcomb <will@dhappy.org>
2015-04-23 14:52:00 -0400

    updating reveal

--- 1436/7/3/index.js
+++ 1436/7/3/index.js
@@ -2,12 +2,12 @@ $(function() {
     function syncSlide( time ) {
 	if( time < 1 ) {
 	    Reveal.slide( 0 )
+	} else if( time < 5 ) {
+	    Reveal.slide( 1, 0 )
 	} else if( time < 10 ) {
-	    Reveal.slide( 1 )
+	    Reveal.slide( 1, 1 )
 	} else if( time < 15 ) {
 	    Reveal.slide( 2 )
-	} else if( time < 20 ) {
-	    Reveal.slide( 3 )
 	} else if( time < 25 ) {
 	    Reveal.slide( 4 )
 	} else if( time < 30 ) {
